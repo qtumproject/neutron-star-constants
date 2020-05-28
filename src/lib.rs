be98d039860bd9a7f2b0625d96b39595a420c36e
Jordan Earls <earlz@earlz.net>
2020-05-28 11:25:33 -0600

    remove no longer needed constants and remove serde dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,56 +1,50 @@
 #![no_std]
-extern crate num_enum;
-use num_enum::TryFromPrimitive;
  
 //Testbench constants are included here as well, though they are not technically part of neutronstar
 
-pub const NEUTRON_INTERRUPT:u8 = 0x40;
-pub const EXIT_INTERRUPT:u8 = 0xF0;
-pub const TESTBENCH_INTERRUPT:u8 = 0x50;
+/*
 
-/// The system calls available using the NEUTRON_INTERRUPT 
-#[derive(Debug, Eq, PartialEq, TryFromPrimitive)]
-#[repr(u32)]
-pub enum NeutronSyscalls{
-    Invalid = 0,
-    /// Pushes a value to the SCCS
-    PushSCCS = 1,
-    /// Pops a value from the SCCS into a fixed size buffer
-    PopSCCS,
-    /// Peeks the value from the top of the SCCS into a fixed size buffer
-    PeekSCCS,
-    /// Pops a value from the SCCS into a dynamically allocated buffer
-    PopSCCSAlloc,
-    /// Peeks the value from the top of the SCCS into a dynamically allocated buffer
-    PeekSCCSAlloc,
-    /// Peeks the size of the value from the top of the SCCS
-    PeekSCCSSize,
-    /// Removes the item from the top of the SCCS without retreiving the value
-    DropSCCS,
-    /// How many items are in the SCCS
-    DepthOfSCCS,
-    /// How much gas has been used up to this point in execution
-    GasUsed,
-    IsCreate = 0xF000
-}
-/// The system calls available using the TESTBENCH_INTERRUPT 
-#[derive(Debug, Eq, PartialEq, TryFromPrimitive)]
-#[repr(u32)]
-pub enum TestbenchSyscalls{
-    Invalid = 0,
-    /// Logs an error message
-    LogError = 1,
-    /// Logs an info message
-    LogInfo,
-    /// Logs a debug message
-    LogDebug,
-    /// Prints a message to the console. Not guaranteed to exist in all circumstances
-    /// and designed primarily for use in testing
-    Print,
-    /// Disables all diagnostics messages excluding crashes.
-    /// Primarily designed for use in testing
-    DisableDiagnostics,
-}
+Summary of interface:
+
+Note: returning u64 values uses the EAX:EDX "mostly but not quite" standard cdcel convention
+Order of registers: EAX, ECX, EDX
+
+-- SCCS functions
+Interrupt 0x10: push_sccs (buffer, size)
+Interrupt 0x11: pop_sccs (buffer, max_size) -> actual_size: u32
+Interrupt 0x12: peek_sccs (buffer, max_size, index) -> actual_size: u32
+Interrupt 0x13: swap_sccs (index)
+Interrupt 0x14: dup_sccs()
+Interrupt 0x15: sccs_item_count() -> size
+Interrupt 0x16: sccs_memory_size() -> size
+Interrupt 0x17: sccs_memory_remaining() -> size
+Interrupt 0x18: sccs_item_limit_remaining() -> size
+
+-- CallSystem functions
+Interrupt 0x20: system_call(feature, function) -> error:u32
+
+-- Hypervisor functions
+Interrupt 0x80: alloc_memory TBD
+
+-- Context functions
+Interrupt 0x90: gas_limit() -> u64
+Interrupt 0x91: self_address() -- result on stack as NeutronShortAddress
+Interrupt 0x92: origin() -- result on stack as NeutronShortAddress
+Interrupt 0x93: origin_long() -- result on stack as NeutronLongAddress
+Interrupt 0x94: sender() -- result on stack as NeutronShortAddress
+Interrupt 0x95: sender_long() -- result on stack as NeutronLongAddress
+Interrupt 0x96: value_sent() -> u64
+Interrupt 0x97: nest_level() -> u32
+Interrupt 0x98: gas_remaining() -> u64
+Interrupt 0x99: execution_type() -> u32
+
+-- System interrupts
+Interrupt 0xFE: revert_execution(status) -> noreturn
+Interrupt 0xFF: exit_execution(status) -> noreturn
+
+
+*/
+
 
 #[derive(Debug, Default, Copy, Clone, Eq, PartialEq, Hash)]
 #[repr(C)]
@@ -65,21 +59,6 @@ pub struct NeutronFullAddress<'a>{
 }
 
 pub const EXEC_FLAG_CREATE: u32 = 1;
-
-#[derive(Debug, Default, Copy, Clone)]
-//#[repr(packed)]
-pub struct NeutronExecContext{
-    pub flags: u64,
-    pub sender: NeutronShortAddress,
-    pub gas_limit: u64,
-    pub value_sent: u64,
-    pub origin: NeutronShortAddress,
-    pub self_address: NeutronShortAddress,
-    pub nest_level: u32
-}
-
-
-
 
 
 
